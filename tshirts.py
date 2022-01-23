90004a20aca6a68249a8588c36f68586042be3aa
Mayya Yashaswin (MS/EVV4-XC) <myy4kor@bosch.com>
2022-01-23 22:12:40 +0530

    Updated tshirt.py and misaligned.py

--- tshirts.py
+++ tshirts.py
@@ -7,8 +7,15 @@ def size(cms):
     else:
         return 'L'
 
+#Assumption is size() requires t-shirt "length" along with "shoulder-measurements"
 
-assert(size(37) == 'S')
-assert(size(40) == 'M')
-assert(size(43) == 'L')
+#Referencefor Test Case:
+# Shoulder      Length          Size
+#   <38           <72            S
+# 38 to 42      72 to 75         M
+#   <42           >75            L
+
+assert(size(37, 71) == 'S')
+assert(size(40, 74) == 'M')
+assert(size(43, 76) == 'L')
 print("All is well (maybe!)\n")
